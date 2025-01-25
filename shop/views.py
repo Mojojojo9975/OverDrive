4588045a87220d902077825e08b9b62e5633e864
Mojojojo9975 <moizkhan9975@gmail.com>
2025-01-26 02:55:32 +0500

    Added Products Listings

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,7 @@ from .models import Product, Category
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
